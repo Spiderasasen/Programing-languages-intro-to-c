1b814b277b60f5ee96056bb1cc20de18f6cee260
spiderasasen <spiderasasen@gmail.com>
2026-02-28 11:42:28 -0500

    made some improvements to seperator

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ void stringCopy(char* in, char* out);
 void reverse(char* string, int size);
 
 int main(void) {
+    seperator(1);
     //number 1, copying a string
     char* string = "ghijkl";
     printf("Original string: %s\n", string);
@@ -20,13 +21,16 @@ int main(void) {
 
     //answer 1
     printf("Copyed String: %s\n", str2);
-    seperator();
+    seperator(2);
 
     //number 2, reverse a string in place (with a given size)
     char string1[6] = "edcba";
     printf("Original string: %s\n", string1);
     reverse(string1, 5);
+
+    //answer 2
     printf("Reversed string: %s\n", string1);
+    seperator(3);
 
     //number 3, Counting the number of times of a given number in a string
     //number 4, print the contents of an array of ints
@@ -39,15 +43,18 @@ int main(void) {
 /*MAKING THE FUNCTIONS*/
 
 //sepreating the questions
-void seperator() {
-    printf("|***************************************************|\n");
+void seperator(int question_number) {
+    printf("\n|************* Question %d *************|\n", question_number);
 }
 
 //question 1
 void stringCopy(char* in, char* out) {
-    for (int i = 0; in[i] != '\0'; i++) {
+    int i = 0;
+    while (in[i] != '\0') {
         out[i] = in[i];
+        i++;
     }
+    out[i] = '\0';
 }
 
 //question 2
