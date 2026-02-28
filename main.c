7a9329f5cb60ba1b47dcf7b509089350774c477c
spiderasasen <spiderasasen@gmail.com>
2026-02-28 11:33:45 -0500

    made a sepreator section so the profe can see the differnace in everything

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 /*FUNCTIONS FOR ALL QUESTIONS GET CALLED HERE*/
+void seperator();
 void stringCopy(char* in, char* out);
 void reverse(char* string, int size);
 
@@ -19,8 +20,14 @@ int main(void) {
 
     //answer 1
     printf("Copyed String: %s\n", str2);
+    seperator();
 
     //number 2, reverse a string in place (with a given size)
+    char string1[6] = "edcba";
+    printf("Original string: %s\n", string1);
+    reverse(string1, 5);
+    printf("Reversed string: %s\n", string1);
+
     //number 3, Counting the number of times of a given number in a string
     //number 4, print the contents of an array of ints
     //number 5, sum the contents of an array of ints
@@ -30,6 +37,11 @@ int main(void) {
 
 
 /*MAKING THE FUNCTIONS*/
+
+//sepreating the questions
+void seperator() {
+    printf("|***************************************************|\n");
+}
 
 //question 1
 void stringCopy(char* in, char* out) {
